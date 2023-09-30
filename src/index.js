b91354de001221b1d765dfc8e5a566c258d0d73d
Hammad Sheikh-Bin-Nadeem <64545966+hammad-air@users.noreply.github.com>
2023-09-30 23:02:50 +0500

    new commit added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { useNavigate, Route, Routes, Router } from 'react-router-dom';
+import { ClerkProvider, RedirectToSignIn, SignIn, SignUp, SignedIn, SignedOut } from '@clerk/clerk-react';
+import ProtectedPage from './ProtectedPage';
 
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const ClerkWithRoutes = () => {
+  const navigate = useNavigate()
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<ProtectedPage />} />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn redirectUrl={'/protected'} routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp redirectUrl={'/protected'} routing="path" path="/sign-up" />}
+        />
+        <Route
+          path="/protected"
+          element={
+            <>
+              <SignedIn>
+                <ProtectedPage />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  )
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ClerkWithRoutes />
+    </Router>
   </React.StrictMode>
 );
 
