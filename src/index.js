10118a4ee489aab2689da564c763d4b4e1006bdd
Hammad Sheikh-Bin-Nadeem <64545966+hammad-air@users.noreply.github.com>
2023-09-30 23:23:18 +0500

    new commit added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { useNavigate, Route, Routes, Router } from 'react-router-dom';
+import { useNavigate, BrowserRouter, Route, Routes } from 'react-router-dom'; // Remove 'Router'
 import { ClerkProvider, RedirectToSignIn, SignIn, SignUp, SignedIn, SignedOut } from '@clerk/clerk-react';
 import ProtectedPage from './ProtectedPage';
 
@@ -49,13 +49,10 @@ const ClerkWithRoutes = () => {
 
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter> {/* Use BrowserRouter here */}
       <ClerkWithRoutes />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
